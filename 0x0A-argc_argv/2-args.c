74e2b47dfab727de64985ad498b1bfd8e754320c
UcheCom <uche4newton1981@gmail.com>
2023-03-12 15:36:18 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function to print its name
+ * @argc:argc  parameter
+ * @argv: an array of commands listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
