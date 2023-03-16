81e9007faf4d99a80bfd2b16a96fc3a16e0df2bd
UcheCom <uche4newton1981@gmail.com>
2023-03-16 14:23:11 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /*
@@ -15,6 +14,7 @@ int _strlen(char *string)
 
 		return (i);
 }
+
 /**
  * string_nconcat - function that concats two strings
  * @s1: string 1
@@ -31,25 +31,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	if (num < 0)
 		return (NULL);
-
 	If(num >= _strlen(s2))
 		num = _strlen(s2);
 
 	len = _strlen(s1) + num + 1;
 
 	ptr = malloc(sizeof(*ptr) * len);
-
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-
 	for (j = 0; j < num; j++)
 		ptr[i + j] = s2[j];
 	ptr[i + j] = '\0';
