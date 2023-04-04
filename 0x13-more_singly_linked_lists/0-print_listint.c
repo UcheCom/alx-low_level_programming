4dde0ab3f3c56b5c85ad0888a195cfcdde32ce9e
Uchenna Oko <uche4newton1981@gmail.com>
2023-04-04 16:12:44 +0000

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * @h: pointer to type listint_t to be printed.
  *
  * Return: number of nodes
  */
