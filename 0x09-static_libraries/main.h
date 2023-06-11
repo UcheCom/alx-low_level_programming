2e8028eb840ce30daa6f0ba9491b1ffe97dfecee
Uchenna Oko <Uche4newton1981@gmail.com>
2023-06-11 11:53:17 +0000

    Header File Updated

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -12,7 +12,6 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
@@ -21,5 +20,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
