68efe1bab8586a70acefe8115bac79bb565719fe
Uchenna Oko <Uche4newton1981@gmail.com>
2023-06-11 00:39:04 +0000

    hot fix

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
  *_strncat - a function that concatenates strings
- *@des: string copied into
+ *@dest: string copied into
  *@src: string to append
  *@n: number of bytes
  *
  *Return: pointer to resulting string destination.
  */
-char *_strncat(char *des, char *src, unsigned int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (des[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (src[j] != '\0' && j < n)
 	{
-		des[i] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	return (des);
+	dest[i] = '\0';
+
+	return (dest);
 }
