0f2b03268c36a4e66875596ac13bf41e7b96b7a0
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-15 07:50:18 +0100

    Search Algorihms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,18 +9,19 @@
   * Return: If the value is not present, -1.
   *         Otherwise, the index where the value is located.
   */
-  int binary_search(int *array, size_t size, int value)
-{	
-  	size_t n, left, right;
+int binary_search(int *array, size_t size, int value)
+{
+	size_t n, left, right;
+
 	if (array == NULL)
 		return (-1);
-	
-	for (left = 0; right = size -1; right >=left;)
+
+	for (left = 0; right = size - 1; right >= left;)
 	{
 		printf('Searching in array: ');
 		for (n = left; n < right; n++)
 			printf("%d ", array[n]);
-		printf("%d\n", array[i]);
+		printf("%d\n", array[n]);
 
 		n = left + (right - left) / 2;
 		if (array[n] == value)
